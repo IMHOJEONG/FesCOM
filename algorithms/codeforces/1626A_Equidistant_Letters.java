6c5c94b4acb0a5b81eb69994a66cf67611af9c34
IMHOJEONG <39ghwjd@naver.com>
2022-01-20 00:26:12 +0900

    chore: 1626A codeforces 문제 풀이

---
+++ algorithms/codeforces/1626A_Equidistant_Letters.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.*;
+ 
+public class A_1627A {
+ 
+	static BufferedReader br;
+	
+	static int toInt(String msg){
+	    return Integer.parseInt(msg);
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int t = toInt(br.readLine());
+        
+        for(int k = 0 ; k < t ; k++){
+            
+            String[] arr = br.readLine().split("");
+            
+            Arrays.sort(arr);
+            
+            
+            System.out.println(String.join("",arr));
+            
+        }
+        
+        
+
+	}
+	
+}  
